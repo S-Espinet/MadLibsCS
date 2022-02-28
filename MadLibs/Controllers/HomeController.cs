0c1e3008b9b16f40cdefb82fecc6d939c15613b4
S. Espinet <s.lou.espinet@gmail.com>
2022-02-28 11:47:06 -0800

    project setup based on FriendLetter

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using MadLibs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadLibs.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/")]
+    public ActionResult Instructions()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
+      return View(myLetterVariable);
+    }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/story")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
+
+  }
+}
